5b218fb0c141bbfe86cc14115b45b96b73c14748
Paweł J. Wal <pawel.wal@codesthq.com>
2017-07-15 17:15:35 +0200

    Namespace routes to /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :authors do
-    resources :books
+  namespace :api do
+    namespace :v1 do
+      resources :authors do
+        resources :books
+      end
+    end
   end
 
   root to: 'authors#index'
