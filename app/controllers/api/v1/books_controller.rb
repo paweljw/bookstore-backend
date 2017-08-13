fcdc27eb3689b82274083b9f6cf15e7a4f2099b7
Paweł J. Wal <pawel.wal@codesthq.com>
2017-08-13 16:27:48 +0200

    Fix a bug in getting author's books

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class BooksController < ApplicationController
-      before_action :set_author, only: %i[show update destroy]
+      before_action :set_author, only: %i[index show update destroy]
       before_action :set_book, only: %i[show update destroy]
 
       def index
